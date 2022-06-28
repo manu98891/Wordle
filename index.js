48d35d9eee707ae28436a4590b1856af782c2970
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-06-28 19:32:05 -0300

    Inicio de matriz de colores

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ window.onload = function(){
         [3,5,4,6,9]
     ]
 
-    for (let index = 0; index < matriz.length; index++) {
-        
-        console.log(index);
-    }
+    // const filas = [fila1, fila2, fila3, fila4, fila5, fila6]
+
+    for (let xRow = 0; xRow < filas.length; xRow++) {
+    console.log(filas[xRow])
+    for (let yColumn = 0; yColumn < 6; yColumn++) {
+        console.log(filas[xRow][yColumn])
+    } }
+    
+    var colores =[
+        [8,7,2,3,5],
+        [5,4,1,8,7],
+        [1,4,9,5,7],
+        [6,7,3,8,4],
+        [5,2,3,7,1],
+        [3,5,4,6,9]
+    ]
 }
