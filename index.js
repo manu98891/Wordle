844f8d0695626802895211896abc80cac6ffa9d4
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-06-28 20:12:56 -0300

    Modificaciones generales de JS

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-function obtenerElementos(){
-    inputs = document.querySelectorAll("input[type=text]");
-}
-var matriz=[
-    [1,2,3,4,5],
-    [2,5,6,7,8],
-    [5,6,4,3,2],
-    [9,4,7,9,7],
-    [2,6,3,8,9],
-    [7,3,8,2,5]
-]
-
 window.onload = () => {
-    obtenerElementos();
-    for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            document.getElementById(`f${i}c${j}`).value = matriz[i][j];
-        }
+
+    var matriz=[
+        [1,2,3,4,5],
+        [2,5,6,7,8],
+        [5,6,4,3,2],
+        [9,4,7,9,7],
+        [2,6,3,8,9],
+        [7,3,8,2,5]
+    ]
+
+    for (let xRow = 0; xRow < matriz.length; xRow++) {
+        console.log(matriz[xRow])
+        for (let yColumn = 0; yColumn < 6; yColumn++) {
+            console.log(matriz[xRow][yColumn])
+        } 
     }
-    inputs.forEach(x => x.addEventListener("keyup",saltarInput));
-    
-}
+        
 
-var colores=[
-    [1,2,3,0,0],
-    [2,2,1,2,1],
-    [1,1,3,1,2],
-    [3,1,0,0,0],
-    [2,1,3,1,3],
-    [1,3,2,2,2]
-]
+    var colores=[
+        [1,2,3,0,0],
+        [2,2,1,2,1],
+        [1,1,3,1,2],
+        [3,1,0,0,0],
+        [2,1,3,1,3],
+        [1,3,2,2,2]
+    ]
 
-function pintarTablero(){
-
+    function pintarTablero(){
+        for (let i = 0; i < colores.length; i++) {
+            for (let j = 0; j < colores[i].length; j++) {
+                
+            }
+        }
+        
+    }
 }
