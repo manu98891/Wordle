88b8e029cda92fabc4c030f6d600a1f9d6907b0a
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-06-28 19:54:23 -0300

    Colores matriz

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-window.onload = function(){
-    var matriz = [
-        [8,7,2,3,5],
-        [5,4,1,8,7],
-        [1,4,9,5,7],
-        [6,7,3,8,4],
-        [5,2,3,7,1],
-        [3,5,4,6,9]
-    ]
+function obtenerElementos(){
+    inputs = document.querySelectorAll("input[type=text]");
+}
+var matriz=[
+    [1,2,3,4,5],
+    [2,5,6,7,8],
+    [5,6,4,3,2],
+    [9,4,7,9,7],
+    [2,6,3,8,9],
+    [7,3,8,2,5]
+]
 
-    // const filas = [fila1, fila2, fila3, fila4, fila5, fila6]
-
-    for (let xRow = 0; xRow < filas.length; xRow++) {
-    console.log(filas[xRow])
-    for (let yColumn = 0; yColumn < 6; yColumn++) {
-        console.log(filas[xRow][yColumn])
-    } }
+window.onload = () => {
+    obtenerElementos();
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            document.getElementById(`f${i}c${j}`).value = matriz[i][j];
+        }
+    }
+    inputs.forEach(x => x.addEventListener("keyup",saltarInput));
     
-    var colores =[
-        [8,7,2,3,5],
-        [5,4,1,8,7],
-        [1,4,9,5,7],
-        [6,7,3,8,4],
-        [5,2,3,7,1],
-        [3,5,4,6,9]
-    ]
+}
+
+var colores=[
+    [1,2,3,0,0],
+    [2,2,1,2,1],
+    [1,1,3,1,2],
+    [3,1,0,0,0],
+    [2,1,3,1,3],
+    [1,3,2,2,2]
+]
+
+function pintarTablero(){
+
 }
